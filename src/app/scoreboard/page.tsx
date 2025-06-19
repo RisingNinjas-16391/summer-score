ee212b1b2e7fa28951c4d38a54c79c35ba4b6cf0
Kenneth Lin <klin134340@gmail.com>
2025-06-19 19:30:23 -0400

    formatting

--- src/app/scoreboard/page.tsx
+++ src/app/scoreboard/page.tsx
@@ -594,7 +594,12 @@ export default function Scoreboard() {
       }}
     >
       <center>
-        <Typography variant="h3" sx={{ fontWeight: "bold" }} component="div" gutterBottom>
+        <Typography
+          variant="h3"
+          sx={{ fontWeight: "bold" }}
+          component="div"
+          gutterBottom
+        >
           {match.match_number}
         </Typography>
       </center>
@@ -609,8 +614,12 @@ export default function Scoreboard() {
               textAlign: "center",
             }}
           >
-            <Typography variant="h2" sx={{ fontWeight: "bold" }}>{match.red_name}</Typography>
-            <Typography variant="h1" sx={{ fontWeight: "bold" }}>{redDisplay}</Typography>
+            <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+              {match.red_name}
+            </Typography>
+            <Typography variant="h1" sx={{ fontWeight: "bold" }}>
+              {redDisplay}
+            </Typography>
           </div>
           <Typography
             className={redStatus !== "⠀" ? "fade-message" : ""}
@@ -634,8 +643,12 @@ export default function Scoreboard() {
               textAlign: "center",
             }}
           >
-            <Typography variant="h2" sx={{ fontWeight: "bold" }}>{match.blue_name}</Typography>
-            <Typography variant="h1" sx={{ fontWeight: "bold" }}>{blueDisplay}</Typography>
+            <Typography variant="h2" sx={{ fontWeight: "bold" }}>
+              {match.blue_name}
+            </Typography>
+            <Typography variant="h1" sx={{ fontWeight: "bold" }}>
+              {blueDisplay}
+            </Typography>
           </div>
           <Typography
             className={blueStatus !== "⠀" ? "fade-message" : ""}
@@ -653,7 +666,10 @@ export default function Scoreboard() {
       </Grid>
 
       <Grid container justifyContent="center" paddingTop={2}>
-        <Typography variant="h1" sx={{ fontSize: "clamp(6rem, 15vw, 12rem)", fontWeight: "bold" }}>
+        <Typography
+          variant="h1"
+          sx={{ fontSize: "clamp(6rem, 15vw, 12rem)", fontWeight: "bold" }}
+        >
           {isInCountdown
             ? `0:${countdown.toString().padStart(2, "0")}`
             : `${Math.floor(timer / 60)}:${(timer % 60)

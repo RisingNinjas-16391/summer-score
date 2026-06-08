60070879074d010a773634159d8f1ce4e51d2fc9
RosarioCast24 <rosarito1224@gmail.com>
2026-06-08 13:56:45 -0400

    Update teamMap.ts

--- src/lib/teamMap.ts
+++ src/lib/teamMap.ts
@@ -1,5 +1,5 @@
 export const teamMap: Record<string, string> = {
-  "Team 1": "Sigma Rizzlers",
+  "Team 1": "Kachow",
   "Team 2": "Ur mom",
   "Team 3": "Steak on a Stake",
   "Team 4": "Shaquille O'Meal",

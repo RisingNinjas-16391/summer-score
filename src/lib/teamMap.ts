13a691535ee26d835e6f39d36b9369125a063a4f
RosarioCast24 <rosarito1224@gmail.com>
2026-06-09 13:23:01 -0400

    update

--- src/lib/teamMap.ts
+++ src/lib/teamMap.ts
@@ -4,5 +4,5 @@ export const teamMap: Record<string, string> = {
   "Team 3": "Beams on a Companion Cube",
   "Team 4": "Eiscreme",
   "Team 5": "The Hip-Hop Minions",
-  "Team 6": "The Spongebob Squarepants",
+
 };

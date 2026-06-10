7b67a0342b10e2ff48296147cd6e8a77b9e9f99c
RosarioCast24 <rosarito1224@gmail.com>
2026-06-10 15:26:57 -0400

    songs

--- src/lib/teamMap.ts
+++ src/lib/teamMap.ts
@@ -3,6 +3,6 @@ export const teamMap: Record<string, string> = {
   "Team 2": "PRAWNs",
   "Team 3": "Beams on a Companion Cube",
   "Team 4": "Eiscreme",
-  "Team 5": "The Hip-Hop Minions",
-
+  "Team 5": "The Eggos",
+  "Team 6": "The Missing Khip Bag",
 };
